1d0fa1b8e8b2ebe68ec4dcbeaaff59df00dc508f
saifA <saifA@Saber.myfiosgateway.com>
2023-08-03 19:40:31 -0400

    added new line

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -12,6 +12,7 @@ public class DemoDay3 {
 
 	public static void add2(int a, int b, int c) {
 		System.out.println(a + b + c);
+		System.out.println(a+b);
 	}
 
 	public static void divide(int a, int b) {
