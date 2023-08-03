a080cce46c8abd2a0e1979f0322a736e797380c2
saifA <saifA@Saber.myfiosgateway.com>
2023-08-03 19:14:03 -0400

    new

---
+++ src/demo/DemoDay3.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class DemoDay3 {
+	public static void main(String[] args) {
+		System.out.println("Demo..");
+		System.out.println("Sara");
+	}
+
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public static void add2(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
+
+	public static void divide(int a, int b) {
+		System.out.println(a / b);
+	}
+}
